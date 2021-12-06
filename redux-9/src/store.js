9834a82444a28e455e7970c7388126a5f97f4b29
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-12-06 23:50:26 +0200

    commit

---
+++ redux-9/src/store.js
@@ -0,0 +1,33 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
+
+export const increment = () => {
+  return { type: INCREMENT };
+};
+export const decrement = () => {
+  return { type: DECREMENT };
+};
+export const reset = () => {
+  return { type: RESET };
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    case RESET:
+      return (state = 0);
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
