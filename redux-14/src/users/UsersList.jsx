a5ea81b66f04c527214dab8222620baf8f5e5c4f
Ivan <kovaletsivan@gmail.com>
2021-12-20 18:01:52 +0200

    commit

--- redux-14/src/users/UsersList.jsx
+++ redux-14/src/users/UsersList.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import User from "./User";
-import Filter from "./Filter";
+import Filter from "../Filter";
 import { getFilterText, usersListToshow } from "../users.selectors";
 import { choiceUsers } from "../users.actions";
 
 const UsersList = ({ usersList, filterText, choiceUsers }) => {
-    
+
  const getUsers =()=>{
     const value = document.querySelector(".filter__input").value;
     choiceUsers(value);
